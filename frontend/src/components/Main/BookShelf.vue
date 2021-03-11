9f9863f531c11fb1859a5b3ecf322512343e20e7
whaudg98 <whaudg98@naver.com>
2021-03-11 21:13:36 +0900

    [S04FIELD04-67] Feat: main page의 책 선반 컴포넌트 생성

---
+++ frontend/src/components/Main/BookShelf.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="book-shelf">
+    <img width="1000px" height="60px" src="@/assets/wood_texture.jpg" alt="" style="border-radius: 10px;">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .book-shelf{
+    text-align: center; margin: 0 auto;
+  }
+</style>
