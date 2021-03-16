66fa046e54f6a909f1e1794181ab252dad447d65
whaudg98 <whaudg98@naver.com>
2021-03-16 16:43:24 +0900

    [S04FIELD04-92] Feat: 이메일 중복 검사

--- frontend/src/components/Signup/SubmitEmail.vue
+++ frontend/src/components/Signup/SubmitEmail.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
     <p class="noticeMessage">가입하실 이메일 주소를 입력해주세요.</p>
-    <b-form-input
-      v-model="email"
-      id="id-input"
-      class="id-input"
-      :class="emailStatus"
-      style="border: 0;
-      border-bottom: 1px solid;
-      border-radius: 0;
-      "
-      placeholder="이메일 (example@gmail.com)"
-    >
-    </b-form-input>
-    <div style="color: red; margin-bottom: -24px;" v-show="emailStatus === 'is-invalid'">
-      이메일 양식이 올바르지 않습니다.
-    </div>
-    <br>
-    <!-- <div @click="setEmail">이거는</div> -->
-    <b-button
-      id="next-btn"
-      :class="btnStatus" 
-      @click="setEmail"
-    >
-      확인
-    </b-button>
+    <b-form>
+      <b-form-input
+        v-model="email"
+        class="id-input"
+        :class="emailStatus"
+        style="border: 0;
+        border-bottom: 1px solid;
+        border-radius: 0;
+        "
+        placeholder="이메일 (example@gmail.com)"
+        @blur="duplicateCheck"
+        @keydown.enter="setEmail"
+      >
+      </b-form-input>
+      <div style="color: red; margin-bottom: -24px;" v-show="emailStatus === 'duplicate'">
+        이미 가입된 회원입니다.
+      </div>
+      <div style="color: red; margin-bottom: -24px;" v-show="emailStatus === 'form-invalid'">
+        이메일 양식이 올바르지 않습니다.
+      </div>
+      <br>
+      <b-button
+        class="btn btn-success btn-block next-btn"
+        :class="able" 
+        @click="setEmail"
+      >
+        확인
+      </b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-let emailValid = "btn btn-success btn-block"
-let emailInvalid = "btn btn-success btn-block disabled"
+// let emailValid = "btn btn-success btn-block"
+// let emailInvalid = "btn btn-success btn-block disabled"
 export default {
   data() {
     return {
@@ -39,9 +44,10 @@ export default {
         size: "back-sm"
       },
       email: "",
-      // 현재 이메일이 유효한지 아닌지 (is-valid, is-invalid)
+      // 현재 이메일이 유효한지 아닌지 ("", form-invalid, duplicate)
       emailStatus: "",
-      btnStatus: emailInvalid,
+      // 다음 단계로 넘어갈 수 있는지 (disabled, "")
+      able: "disabled",
     }
   },
   created() {
@@ -60,23 +66,47 @@ export default {
     isValidEmail() {
       const reg_email = /^([0-9a-zA-Z_\\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
       if (this.email && !reg_email.test(this.email)) {
-        this.emailStatus = "is-invalid"
-        this.btnStatus = emailInvalid
+        this.emailStatus = "form-invalid"
+        this.able = "disabled"
       } else {
         if (this.email.length > 0) {
-          this.emailStatus = "is-valid"
-          this.btnStatus = emailValid
+          this.emailStatus = ""
+          this.able = ""
         } else {
           this.emailStatus = ""
-          this.btnStatus = emailInvalid
+          this.able = "disabled"
         }
       }
     },
     setEmail() {
-      console.log('커밋 완료')
+      // 여기서 가입된 이메일이면 alert
+      this.duplicateCheck()
       const user = {userEmail: this.email}
       this.$store.dispatch("SetEmail", user)
       this.$router.push({ name: 'CheckEmail' })
+    },
+    duplicateCheck() {
+      // console.log(`${this.$store.getters.getServer}/user/check`, '주소')
+      // console.log(this.email, '이메일')
+      this.$axios.get(`${this.$store.getters.getServer}/user/check`, {
+        params: {
+          userEmail: this.email
+        }
+      })
+      .then(res => {
+        // console.log(res.data)
+        // 가입 불가능
+        if (!res.data.success) {
+          this.emailStatus = "duplicate"
+          this.able = "disabled"
+        } else {
+          this.emailStatus = ""
+          this.able = ""
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
     }
   }
 }
@@ -87,11 +117,7 @@ export default {
     color: rgb(108, 160, 29);
     margin-top: 20px;
   }
-  /* #id-input[type="text"]:focus {
-    box-shadow: 0 0px 0px rgba(0, 0, 0, 0.075);
-    outline: 0 none;
-  } */
-  #next-btn{
+  .next-btn{
     margin-top: 20px;
   }
   .form-control:focus{
