76083997387952aee47a160e10cbd7a60a4ba1ef
whaudg98 <whaudg98@naver.com>
2021-03-31 04:42:28 +0900

    [S04FIELD04-215] Feat: fontawesome 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../public/reset.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
 // import 'fulllpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow: true
 // import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
 import VueFullPage from 'vue-fullpage.js'
