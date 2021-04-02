1067ef73cd03fec6879544eff20379b3ceaaca65
whaudg98 <whaudg98@naver.com>
2021-04-02 11:37:19 +0900

    [S04FIELD04-176] Fix: 선호장르 그래프 커스터마이징

--- frontend/src/components/Charts/PreferenceChart.vue
+++ frontend/src/components/Charts/PreferenceChart.vue
@@ -11,25 +11,33 @@
 <script>
 
 export default {
+  data() {
+    return {
+
+    }
+  },
   mounted() {
     Highcharts.chart('container', {
       chart: {
         type: 'packedbubble',
-        height: '100%'
+        height: '100%',
+        backgroundColor: '#EDEAE8'
       },
       title: {
-        text: 'Carbon emissions around the world (2014)'
+        text: null
       },
       tooltip: {
-        useHTML: true,
-        pointFormat: '<b>{point.name}:</b> {point.value}m CO<sub>2</sub>'
+        followPointer: true,
+        // useHTML: true,
+        pointFormat: '<b>{point.name}:</b> {point.value}'
       },
       plotOptions: {
         packedbubble: {
-          minSize: '30%',
-          maxSize: '120%',
+          // zMin은 0, zMax는 100, minSize는 30%, maxSize는 120%
+          minSize: '15%',
+          maxSize: '130%',
           zMin: 0,
-          zMax: 1000,
+          zMax: 100,
           layoutAlgorithm: {
             splitSeries: false,
             gravitationalConstant: 0.02
@@ -38,9 +46,10 @@ export default {
             enabled: true,
             format: '{point.name}',
             filter: {
+              // y value가 10이상인 것만 보여주겠다 (인덱스 이름)
               property: 'y',
               operator: '>',
-              value: 250
+              value: 10
             },
             style: {
               color: 'black',
@@ -51,476 +60,133 @@ export default {
         }
       },
       series: [{
-        name: 'Europe',
+        name: '취미/여가',
         data: [{
-          name: 'Germany',
-          value: 767.1
+          name: '가정/요리/뷰티',
+          value: 76.1
         }, {
-          name: 'Croatia',
+          name: '건강/취미/레저',
           value: 20.7
         },
         {
-          name: "Belgium",
-          value: 97.2
-        },
-        {
-          name: "Czech Republic",
-          value: 111.7
-        },
-        {
-          name: "Netherlands",
-          value: 158.1
-        },
-        {
-          name: "Spain",
-          value: 241.6
-        },
-        {
-          name: "Ukraine",
-          value: 249.1
-        },
-        {
-          name: "Poland",
-          value: 298.1
-        },
-        {
-          name: "France",
-          value: 323.7
-        },
-        {
-          name: "Romania",
-          value: 78.3
-        },
-        {
-          name: "United Kingdom",
-          value: 415.4
-        }, {
-          name: "Turkey",
-          value: 353.2
-        }, {
-          name: "Italy",
-          value: 337.6
-        },
-        {
-          name: "Greece",
-          value: 71.1
-        },
-        {
-          name: "Austria",
-          value: 69.8
-        },
-        {
-          name: "Belarus",
-          value: 67.7
-        },
-        {
-          name: "Serbia",
-          value: 59.3
-        },
-        {
-          name: "Finland",
-          value: 54.8
-        },
-        {
-          name: "Bulgaria",
-          value: 51.2
-        },
-        {
-          name: "Portugal",
-          value: 48.3
-        },
-        {
-          name: "Norway",
-          value: 44.4
-        },
-        {
-          name: "Sweden",
-          value: 44.3
-        },
-        {
-          name: "Hungary",
-          value: 43.7
-        },
-        {
-          name: "Switzerland",
-          value: 40.2
-        },
-        {
-          name: "Denmark",
-          value: 40
-        },
-        {
-          name: "Slovakia",
-          value: 34.7
-        },
-        {
-          name: "Ireland",
-          value: 34.6
-        },
-        {
-          name: "Croatia",
-          value: 20.7
-        },
-        {
-          name: "Estonia",
-          value: 19.4
-        },
-        {
-          name: "Slovenia",
-          value: 16.7
-        },
-        {
-          name: "Lithuania",
-          value: 12.3
-        },
-        {
-          name: "Luxembourg",
-          value: 10.4
-        },
-        {
-          name: "Macedonia",
-          value: 9.5
-        },
-        {
-          name: "Moldova",
-          value: 7.8
-        },
-        {
-          name: "Latvia",
-          value: 7.5
-        },
-        {
-          name: "Cyprus",
-          value: 7.2
-        }]
-      }, {
-        name: 'Africa',
-        data: [{
-          name: "Senegal",
-          value: 8.2
-        },
-        {
-          name: "Cameroon",
+          name: "여행",
           value: 9.2
         },
         {
-          name: "Zimbabwe",
+          name: "잡지",
+          value: 11.7
+        }]
+      }, {
+        name: '문학',
+        data: [{
+          name: "고전",
+          value: 8.2
+        },
+        {
+          name: "소설/시/희곡",
+          value: 9.2
+        },
+        {
+          name: "에세이",
           value: 13.1
         },
         {
-          name: "Ghana",
+          name: "장르소설",
           value: 14.1
-        },
-        {
-          name: "Kenya",
-          value: 14.1
-        },
-        {
-          name: "Sudan",
-          value: 17.3
-        },
-        {
-          name: "Tunisia",
-          value: 24.3
-        },
-        {
-          name: "Angola",
-          value: 25
-        },
-        {
-          name: "Libya",
-          value: 50.6
-        },
-        {
-          name: "Ivory Coast",
-          value: 7.3
-        },
-        {
-          name: "Morocco",
-          value: 60.7
-        },
-        {
-          name: "Ethiopia",
-          value: 8.9
-        },
-        {
-          name: "United Republic of Tanzania",
-          value: 9.1
-        },
-        {
-          name: "Nigeria",
-          value: 93.9
-        },
-        {
-          name: "South Africa",
-          value: 392.7
-        }, {
-          name: "Egypt",
-          value: 225.1
-        }, {
-          name: "Algeria",
-          value: 141.5
         }]
       }, {
-        name: 'Oceania',
+        name: '유아/어린이',
         data: [{
-          name: "Australia",
-          value: 409.4
+          name: "유아",
+          value: 40.4
         },
         {
-          name: "New Zealand",
+          name: "어린이",
           value: 34.1
         },
         {
-          name: "Papua New Guinea",
+          name: "좋은부모",
           value: 7.1
         }]
       }, {
-        name: 'North America',
+        name: '경제/사회',
         data: [{
-          name: "Costa Rica",
+          name: "경제경영",
           value: 7.6
         },
         {
-          name: "Honduras",
+          name: "사회과학",
           value: 8.4
-        },
-        {
-          name: "Jamaica",
-          value: 8.3
-        },
-        {
-          name: "Panama",
-          value: 10.2
-        },
-        {
-          name: "Guatemala",
-          value: 12
-        },
-        {
-          name: "Dominican Republic",
-          value: 23.4
-        },
-        {
-          name: "Cuba",
-          value: 30.2
-        },
-        {
-          name: "USA",
-          value: 5334.5
-        }, {
-          name: "Canada",
-          value: 566
-        }, {
-          name: "Mexico",
-          value: 456.3
         }]
       }, {
-        name: 'South America',
+        name: '수험서/전공서적',
         data: [{
-          name: "El Salvador",
+          name: "대학교재/전문서적",
           value: 7.2
         },
         {
-          name: "Uruguay",
+          name: "수험서/자격증",
           value: 8.1
         },
         {
-          name: "Bolivia",
+          name: "외국어",
           value: 17.8
         },
         {
-          name: "Trinidad and Tobago",
+          name: "초등학교참고서",
           value: 34
         },
         {
-          name: "Ecuador",
+          name: "중학교참고서",
           value: 43
         },
         {
-          name: "Chile",
-          value: 78.6
-        },
-        {
-          name: "Peru",
-          value: 52
-        },
-        {
-          name: "Colombia",
-          value: 74.1
-        },
-        {
-          name: "Brazil",
-          value: 501.1
-        }, {
-          name: "Argentina",
-          value: 199
-        },
-        {
-          name: "Venezuela",
-          value: 195.2
+          name: "고등학교참고서",
+          value: 7.6
         }]
       }, {
-        name: 'Asia',
+        name: '청소년/만화',
         data: [{
-          name: "Nepal",
+          name: "만화",
           value: 6.5
         },
         {
-          name: "Georgia",
+          name: "청소년",
+          value: 6.5
+        }]
+      }, {
+        name: '인문/예술/역사',
+        data: [{
+          name: "인문학",
           value: 6.5
         },
         {
-          name: "Brunei Darussalam",
+          name: "종교/역학",
+          value: 6.5
+        },
+        {
+          name: "예술/대중문화",
           value: 7.4
         },
         {
-          name: "Kyrgyzstan",
+          name: "역사",
           value: 7.4
+        }]
+      }, {
+        name: '자기계발',
+        data: [{
+          name: "자기계발",
+          value: 6.5
+        }]
+      }, {
+        name: 'IT/과학',
+        data: [{
+          name: "과학",
+          value: 6.5
         },
         {
-          name: "Afghanistan",
-          value: 7.9
-        },
-        {
-          name: "Myanmar",
-          value: 9.1
-        },
-        {
-          name: "Mongolia",
-          value: 14.7
-        },
-        {
-          name: "Sri Lanka",
-          value: 16.6
-        },
-        {
-          name: "Bahrain",
-          value: 20.5
-        },
-        {
-          name: "Yemen",
-          value: 22.6
-        },
-        {
-          name: "Jordan",
-          value: 22.3
-        },
-        {
-          name: "Lebanon",
-          value: 21.1
-        },
-        {
-          name: "Azerbaijan",
-          value: 31.7
-        },
-        {
-          name: "Singapore",
-          value: 47.8
-        },
-        {
-          name: "Hong Kong",
-          value: 49.9
-        },
-        {
-          name: "Syria",
-          value: 52.7
-        },
-        {
-          name: "DPR Korea",
-          value: 59.9
-        },
-        {
-          name: "Israel",
-          value: 64.8
-        },
-        {
-          name: "Turkmenistan",
-          value: 70.6
-        },
-        {
-          name: "Oman",
-          value: 74.3
-        },
-        {
-          name: "Qatar",
-          value: 88.8
-        },
-        {
-          name: "Philippines",
-          value: 96.9
-        },
-        {
-          name: "Kuwait",
-          value: 98.6
-        },
-        {
-          name: "Uzbekistan",
-          value: 122.6
-        },
-        {
-          name: "Iraq",
-          value: 139.9
-        },
-        {
-          name: "Pakistan",
-          value: 158.1
-        },
-        {
-          name: "Vietnam",
-          value: 190.2
-        },
-        {
-          name: "United Arab Emirates",
-          value: 201.1
-        },
-        {
-          name: "Malaysia",
-          value: 227.5
-        },
-        {
-          name: "Kazakhstan",
-          value: 236.2
-        },
-        {
-          name: "Thailand",
-          value: 272
-        },
-        {
-          name: "Taiwan",
-          value: 276.7
-        },
-        {
-          name: "Indonesia",
-          value: 453
-        },
-        {
-          name: "Saudi Arabia",
-          value: 494.8
-        },
-        {
-          name: "Japan",
-          value: 1278.9
-        },
-        {
-          name: "China",
-          value: 10540.8
-        },
-        {
-          name: "India",
-          value: 2341.9
-        },
-        {
-          name: "Russia",
-          value: 1766.4
-        },
-        {
-          name: "Iran",
-          value: 618.2
-        },
-        {
-          name: "Korea",
-          value: 610.1
+          name: "컴퓨터/모바일",
+          value: 6.5
         }]
       }]
     });
