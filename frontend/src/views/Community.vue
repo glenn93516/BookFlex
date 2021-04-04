620ef227a457cc33ae62750cbe48bbc1650bb256
whaudg98 <whaudg98@naver.com>
2021-04-04 23:26:25 +0900

    [S04FIELD04-276] Feat: community 초안 생성
    Former-commit-id: 5e8683602a738993b71cc809440c1a52e06065bd

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -4,28 +4,46 @@
       <header class="title">이달의 추천 책</header>
       <hr>
       <div>
-        <book></book>
+        <!-- <Book></Book> -->
       </div>
     </section>
     <section id="sentence-timeline">
       <header class="title">실시간 수집된 문장</header>
       <hr>
+      <!-- <community-item :item="showItems[0]"></community-item> -->
+      <div id="itemContainer">
+        <community-item v-for="item in showItems" :key="item.index" :item="item"></community-item>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 // import Book from '@/components/Main/Book.vue'
+import CommunityItem from '../components/Community/CommunityItem.vue'
 
 export default {
   components: {
     // Book,
+    CommunityItem,
   },
   data() {
-    return {}
+    return {
+      showItems: [],
+    }
   },
   methods: {
 
+  },
+  created() {
+    this.$axios.get(`${this.$store.getters.getServer}/highlight`)
+    .then(res => {
+      console.log(res.data.data, 'res')
+      this.showItems = res.data.data
+    })
+    .catch(err => {
+      console.log(err)
+    })
   },
 }
 </script>
