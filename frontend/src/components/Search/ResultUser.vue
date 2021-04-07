adb3112b2538a1dbdac70ea1ce5fe2c9f541e4aa
whaudg98 <whaudg98@naver.com>
2021-04-07 15:35:25 +0900

    [S04FIELD04-331] Feat: 결과 페이지 수정
    Former-commit-id: 64c4d69447a31178be0d0b4b45c9ee2e1525a2b6

--- frontend/src/components/Search/ResultUser.vue
+++ frontend/src/components/Search/ResultUser.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h2><strong>검색 결과 - USER</strong></h2>
+    <!-- <div id="pointer-pink" class="pointer"> -->
+    <h2 class="result-title">USER</h2>
+    <!-- </div> -->
     <div v-if="userData==null">
       <h4 style="text-align: center; ">😥검색된 결과가 없습니다.</h4>
     </div>
@@ -23,5 +25,40 @@ export default {
 </script>
 
 <style>
-
+  #pointer-pink {
+    width: 140px;
+    height: 40px;
+    position: relative;
+    background: rgba(146, 208, 80);
+    align-items: center;
+  }
+  .pointer:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 0;
+    height: 0;
+    border-left: 20px solid rgba(237, 234, 232, 1);
+    border-top: 20px solid transparent;
+    border-bottom: 20px solid transparent;
+    align-items: center;
+  }
+  #pointer-pink:before {
+    content: "";
+    position: absolute;
+    right: -20px;
+    bottom: 0;
+    width: 0;
+    height: 0;
+    border-left: 20px solid rgba(146, 208, 80);
+    border-top: 20px solid transparent;
+    border-bottom: 20px solid transparent;
+    align-items: center;
+  }
+  .result-title {
+    font-weight: bold;
+    /* margin-left: 30px; */
+    /* line-height: inherit; */
+  }
 </style>
